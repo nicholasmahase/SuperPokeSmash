d82d45ddf35bb58bfedc95bec8d7264981cbc76c
Nicholas Mahase <nicholasmahase@gmail.com>
2019-03-14 23:50:44 -0400

    delete

---
+++ spsuser.sql
@@ -0,0 +1,6 @@
+-- Drops the user if it exists currently --
+DROP DATABASE IF EXISTS spsuser;
+-- Creates the "user" database --
+CREATE DATABASE spsuser;
+-- Uses the "user" database --
+USE spsuser;
