342a83cea060d329038bd57b013351e1bfa3d1c3
Nicholas Mahase <nicholasmahase@gmail.com>
2019-03-14 23:56:30 -0400

    updates

--- spsuser.sql
+++
@@ -1,6 +0,0 @@
--- Drops the user if it exists currently --
-DROP DATABASE IF EXISTS spsuser;
--- Creates the "user" database --
-CREATE DATABASE spsuser;
--- Uses the "user" database --
-USE spsuser;
