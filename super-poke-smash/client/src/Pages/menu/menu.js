80e331483e03e8074b5da0a786aa984fba8cff6f
Nicholas Mahase <nicholasmahase@gmail.com>
2019-03-18 19:19:12 -0400

    Menu Page

---
+++ super-poke-smash/client/src/Pages/menu/menu.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import "./menu.css";
+export default class Menu extends Component {
+    
+    render() {
+    return (
+    <div>
+        <div className="container">
+            <div className="row">
+            <h1 className="page title">Menu</h1>
+            </div>
+        </div>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <div className="menulist">
+                        <ul>
+                            <li><a className="startgame" href="/mode">Start Game</a></li>
+                            <li><a className="pokedex" href="/pokedex">Pokedex</a></li>
+                            <li><a className="rules" href="/rules">Rules</a></li>
+                            <li><a className="about" href="/about">About</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>    
+ 
+  );
+}
+}
+
+
