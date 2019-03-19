b42f22b542b5329756203564f059c09a09f80eba
Nicholas Mahase <nicholasmahase@gmail.com>
2019-03-18 20:01:29 -0400

    class updates

--- super-poke-smash/client/src/Pages/mode/mode.js
+++ super-poke-smash/client/src/Pages/mode/mode.js
@@ -7,8 +7,8 @@ export default class Mode extends Component {
    <div>
      <div className="jumbotron">
        <h1>Game Mode</h1>
-       <button type="button" id="singleBtn" class="btn btn-primary">Single Player</button>
-       <button type="button" id="multiBtn" class="btn btn-success">Multiplayer</button>
+       <button type="button" id="singleBtn" className="btn btn-primary">Single Player</button>
+       <button type="button" id="multiBtn" className="btn btn-success">Multiplayer</button>
        </div>
        </div>
  );
