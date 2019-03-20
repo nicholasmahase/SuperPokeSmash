eeced82c1d66fd5f63af74745977c4402c43faa6
Mamoo2018 <rnarain1015@gmail.com>
2019-03-20 00:47:04 -0400

    adding selection page

---
+++ super-poke-smash/client/src/Pages/selection/selection.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+import pokeman from "../../data/pokemon_data";
+
+class Selection extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokeman: []
+    };
+  }
+
+  componentDidMount = () => {
+    this.setState({ pokeman });
+  };
+  render() {
+    console.log(this.state.pokeman);
+
+    return (
+      <React.Fragment>
+        {this.state.pokeman.map((item, index) => (
+          <div key={index}>
+            {item.id} --- {item.label}
+            <img src={item.sprite} />
+          </div>
+        ))}
+      </React.Fragment>
+    );
+  }
+}
+
+export default Selection;
