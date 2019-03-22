6a9dc4f41e76810d6867b0dfb700519fb2338c8d
Nicholas Mahase <nicholasmahase@gmail.com>
2019-03-22 17:19:38 -0400

    pokedex updates

--- super-poke-smash/client/src/Pages/pokedex/pokedex.js
+++ super-poke-smash/client/src/Pages/pokedex/pokedex.js
@@ -1,60 +1,40 @@
 import React, { Component } from "react";
-import "./pokedex.css";
-
-var PokedexA = require('pokedex'),
-pokedex = new PokedexA();
-
-console.log( pokedex.pokemon()[0] );
-// document.write( pokedex.pokemon()[0].name );
-// document.write( pokedex.pokemon()[0].sprites.normal );
-
-
+import PokeList from './PokeList';
+import DetailView from './DetailView';
+import Pokemon from './Pokemon';
+import "./styles/pokedex.css";
 
 export default class Pokedex extends Component {
 
-    render() {
-    return (
-        
-    <div>
-        <div className="container">
-            <div className="row">
-            <h1 className="page title">Pokedex</h1>
-            </div>
-        </div>
-            <table className="table">
-    <thead className="thead-dark">
-        <tr>
-        <th scope="col">ID</th>
-        <th scope="col">Name</th>
-        <th scope="col">Last</th>
-        <th scope="col">Handle</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-        <th scope="row">1</th>
-        <td></td>
-        <td>Otto</td>
-        <td>@mdo</td>
-        </tr>
-        <tr>
-        <th scope="row">2</th>
-        <td>Jacob</td>
-        <td>Thornton</td>
-        <td>@fat</td>
-        </tr>
-        <tr>
-        <th scope="row">3</th>
-        <td>Larry</td>
-        <td>the Bird</td>
-        <td>@twitter</td>
-        </tr>
-    </tbody>
-    </table>
-</div>    
- 
-  );
-}
-}
+    constructor() {
+        super();
+        this.state = {
+            pokemon: {}
+        };
+
+        this.handleOnClick = this.handleOnClick.bind(this);
+
+      }
+
+     // Inside the App class
+     handleOnClick(id) {
+        fetch(`http://pokeapi.co/api/v2/pokemon/${id}/`)
+          .then(res => res.json())
+          .then(data => {
+            const pokemon = new Pokemon(data);
+    
+            this.setState({ pokemon });
+          })
+          .catch(err => console.log(err));
+      }
+      render() {
+        return (
+          <div className="pokedex">
+            <PokeList handleOnClick={this.handleOnClick} />
+            <DetailView pokemon={this.state.pokemon} />
+          </div>
+        );
+      }
+    }
 
 
