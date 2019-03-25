d1c26a51039071a32d6d24b1b7217560ca92e694
Carly Farrugia <carlyfarrugia@Carlys-MacBook-Pro.local>
2019-03-25 01:40:30 -0400

    users

--- super-poke-smash/client/src/Signin/Signin.js
+++ super-poke-smash/client/src/Signin/Signin.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "./Signin.css";
 
+import axios from "axios";
+
 export default class Signin extends Component {
   constructor(props) {
     super(props);
@@ -17,49 +19,97 @@ export default class Signin extends Component {
 
   handleChange = event => {
     this.setState({
-      [event.target.id]: event.target.value
+      [event.target.name]: event.target.value
     });
-  }
+  };
 
   handleSubmit = event => {
     event.preventDefault();
-  }
+    // console.log(this.state);
+    axios
+      .post("/api/users/login", { user: this.state })
+      .then(function(res) {
+        //Fisrt we want to authenticate him,
+        //We got to store the token in the localstorage
+        // window.location.href = "./signin";
+        //Save to localStorage
+        const { token } = res.data;
+        // Set to localStorage
+        localStorage.setItem("jwtToken", token);
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  };
 
   render() {
     return (
-    
-    <div className="container">
-    <div className="d-flex justify-content-center h-100">
-      <div className="card">
-        <div className="card-header">
-          <h1>Sign In</h1>
-        </div>
-        <div className="card-body">
-          <form id="signin" name="signin" method="post" action="/mode">
-          <div className="col-xs-4">
-              <div className="input-group-prepend">
-                <span className="input-group-text"><i className="fas fa-user"></i></span>
-              </div>
-              <input type="text" className="form-control" name="username" id="username" placeholder="username" />
+      <div className="container">
+        <div className="d-flex justify-content-center h-100">
+          <div className="card">
+            <div className="card-header">
+              <h1>Sign In</h1>
             </div>
-            <div className="col-xs-4">
-              <div className="input-group-prepend">
-                <span className="input-group-text"><i class="fas fa-key"></i></span>
-              </div>
-              <input type="password" className="form-control" name="password" id="password" placeholder="password" />
+            <div className="card-body">
+              <form
+                id="signin"
+                name="signin"
+                method="post"
+                onSubmit={this.handleSubmit}
+              >
+                <div className="col-xs-4">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text">
+                      <i className="fas fa-user" />
+                    </span>
+                  </div>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="username"
+                    id="username"
+                    placeholder="username"
+                    value={this.state.username}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="col-xs-4">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text">
+                      <i className="fas fa-key" />
+                    </span>
+                  </div>
+                  <input
+                    type="password"
+                    className="form-control"
+                    name="password"
+                    id="password"
+                    placeholder="password"
+                    value={this.state.password}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="col-xs-4">
+                  <button
+                    type="submit"
+                    className="btn btn-primary btn-lg signup"
+                  >
+                    Sign In
+                  </button>
+                </div>
+              </form>
             </div>
-            <div className="col-xs-4"><button type="submit" className="btn btn-primary btn-lg signup">Sign In</button></div>
-
-          </form>
-        </div>
-        <div className="card-footer">
-          <div className="d-flex justify-content-center links">
-            Don't have an account? <a className="loginlink" href="/">Sign Up</a>
+            <div className="card-footer">
+              <div className="d-flex justify-content-center links">
+                Don't have an account?{" "}
+                <a className="loginlink" href="/">
+                  Sign Up
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-  );
-}
+    );
+  }
 }
--- super-poke-smash/client/src/Signup/Signup.js
+++ super-poke-smash/client/src/Signup/Signup.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "./Signup.css";
 
+import axios from "axios";
+
 export default class Signup extends Component {
   constructor(props) {
     super(props);
@@ -20,46 +22,119 @@ export default class Signup extends Component {
 
   handleChange = event => {
     this.setState({
-      [event.target.id]: event.target.value
+      [event.target.name]: event.target.value
     });
-  }
+  };
 
   handleSubmit = event => {
     event.preventDefault();
     //AXion POST REQUEST
+    // console.log(this.state);
     //TODO POST REQUEST TO /api/users/register
-  }
+    axios
+      .post("/api/users/register", { user: this.state })
+      .then(function(res) {
+        console.log(res);
+        //Once registerd redirect them to /login
+        window.location.href = "./signin";
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  };
 
   render() {
     return (
       <div className="container">
-      <div className="d-flex justify-content-center h-100">
+        <div className="d-flex justify-content-center h-100">
           <div className="card">
-              <div className="card-header"><h1>Sign Up</h1></div>
-              <p>Please fill in this form to create an account!</p>
+            <div className="card-header">
+              <h1>Sign Up</h1>
+            </div>
+            <p>Please fill in this form to create an account!</p>
           </div>
 
-  <form id="signup" name="signup" method="post" action="/mode">
-              <div className="form-group">
-    <div className="row">
-      <div className="col-xs-4"><input type="text" className="text form-control" name="firstname" placeholder="First Name" required="required" /></div>
-      <div className="col-xs-4"><input type="text" className="text form-control" name="lastname" placeholder="Last Name" required="required" /></div>
-       <div className="col-xs-4"><input type="text" className="text form-control" name="username" placeholder="Username" required="required" /></div>
-       <div className="col-xs-4"><input type="email" className="text form-control" name="email" placeholder="Email" required="required" /></div>
-       <div className="col-xs-4"><input type="password" className="text form-control" name="password" placeholder="Password" required="required" /></div>        
-       <div className="col-xs-4"><button type="submit" className="btn btn-primary btn-lg signup">Sign Up</button></div>
-    </div>        	
+          <form id="signup" name="signup" onSubmit={this.handleSubmit}>
+            <div className="form-group">
+              <div className="row">
+                <div className="col-xs-4">
+                  <input
+                    type="text"
+                    className="text form-control"
+                    name="firstname"
+                    placeholder="First Name"
+                    value={this.state.firstname}
+                    onChange={this.handleChange}
+                    required="required"
+                  />
+                </div>
+                <div className="col-xs-4">
+                  <input
+                    type="text"
+                    className="text form-control"
+                    name="lastname"
+                    value={this.state.lastname}
+                    onChange={this.handleChange}
+                    placeholder="Last Name"
+                    required="required"
+                  />
+                </div>
+                <div className="col-xs-4">
+                  <input
+                    type="text"
+                    className="text form-control"
+                    name="username"
+                    value={this.state.username}
+                    onChange={this.handleChange}
+                    placeholder="Username"
+                    required="required"
+                  />
+                </div>
+                <div className="col-xs-4">
+                  <input
+                    type="email"
+                    className="text form-control"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.handleChange}
+                    placeholder="Email"
+                    required="required"
+                  />
+                </div>
+                <div className="col-xs-4">
+                  <input
+                    type="password"
+                    className="text form-control"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.handleChange}
+                    placeholder="Password"
+                    required="required"
+                  />
+                </div>
+                <div className="col-xs-4">
+                  <button
+                    type="submit"
+                    className="btn btn-primary btn-lg signup"
+                  >
+                    Sign Up
+                  </button>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div className="card-footer">
+          <div className="d-flex justify-content-center links">
+            <div className="hint-text">
+              Already have an account?{" "}
+              <a className="loginlink" href="/signin">
+                Login here
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-
-  </form>
-      </div>
-  <div className="card-footer">
-      <div className="d-flex justify-content-center links">
-          <div className="hint-text">Already have an account? <a className="loginlink" href="/signin">Login here</a></div>
-      </div>
-  </div>
-
-  </div> 
     );
   }
 }
