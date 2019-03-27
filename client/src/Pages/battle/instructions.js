4fc73ce701539bc2ce2719e2621457e6709a3631
Nicholas Mahase <nicholasmahase@gmail.com>
2019-03-27 00:45:39 -0400

    functionailty updates

---
+++ client/src/Pages/battle/instructions.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./battlepage.js";
+import "./battle.css";
+
+class Instructions extends Component {
+  render() {
+    return (
+      <div className="instructions">
+        <p>Choose your Pokemon</p>
+      </div>
+    );
+  }
+}
+export default Instructions;
--- client/src/router.js
+++ client/src/router.js
@@ -1,74 +1,70 @@
-import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
-import selection from './Pages/selection/selection.js';
-import battle from './Pages/battle/battlepage.js';
-import {connect} from 'react-redux';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import selection from "./Pages/selection/selection.js";
+// import battle from './Pages/battle/battlepage.js';
+import { connect } from "react-redux";
 import localForage from "localforage";
-import axios from 'axios';
-import {loadApi} from './actions/playerActions';
-import { BrowserRouter } from 'react-router-dom';
+import axios from "axios";
+import { loadApi } from "./actions/playerActions";
+import { BrowserRouter } from "react-router-dom";
 import Signup from "./Signup/Signup";
 import Signin from "./Signin/Signin";
-import About from './Pages/about/about.js';
-import Arena from './Pages/arena/arena.js';
-import Battle from './Pages/battle/battlepage.js';
-import Menu from './Pages/menu/menu.js';
-import Mode from './Pages/mode/mode.js';
-import Pokedex from './Pages/pokedex/pokedex.js';
-import Rules from './Pages/rules/rules.js';
-import Selection from './Pages/selection/selection.js';
+import About from "./Pages/about/about.js";
+import Arena from "./Pages/arena/arena.js";
+import Battle from "./Pages/battle/battle.js";
+import Menu from "./Pages/menu/menu.js";
+import Mode from "./Pages/mode/mode.js";
+import Pokedex from "./Pages/pokedex/pokedex.js";
+import Rules from "./Pages/rules/rules.js";
+import Selection from "./Pages/selection/selection.js";
 import LiveChat from "./livechat/chat";
 
-
-const mapStateToProps = (state) => ({ api: state.api });
+const mapStateToProps = state => ({ api: state.api });
 
 class Router extends Component {
-  
   constructor(props) {
     super(props);
-    localForage.getItem('backends').then(value => {
-      if (value) { return props.dispatch(loadApi(value)) }
+    localForage.getItem("backends").then(value => {
+      if (value) {
+        return props.dispatch(loadApi(value));
+      }
       axios.get(props.api.url).then(response => {
         let data = {
-          selected: response.data[0],          
-          engines: response.data          
+          selected: response.data[0],
+          engines: response.data
         };
-        localForage.setItem('backends', data);
+        localForage.setItem("backends", data);
         props.dispatch(loadApi(data));
-      });  
-    }); 
+      });
+    });
   }
 
   render() {
     return (
       <div>
-        {
-          this.props.api.selected ? 
-            <BrowserRouter>
-              <Switch>
-                <Route exact path='/selection' component={selection}/>
-                <Route exact path="/" component={Signup} />
-                <Route exact path="/signin" component={Signin} />
-                <Route path="/about" component={About} />
-                <Route path="/chat" component={LiveChat} />
-                <Route path="/arena" component={Arena} />
-                <Route path="/battle" component={Battle} />
-                <Route path="/menu" component={Menu} />
-                <Route path="/mode" component={Mode} />
-                <Route path="/pokedex" component={Pokedex} />
-                <Route path="/rules" component={Rules} />
-                <Route path="/selection" component={Selection} />
-              </Switch> 
-            </BrowserRouter>
-          : 
-            <div style={{padding: '10px'}}>App Will Start Shortly ...</div>
-        }
-      </div>	
+        {this.props.api.selected ? (
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/selection" component={selection} />
+              <Route exact path="/" component={Signup} />
+              <Route exact path="/signin" component={Signin} />
+              <Route path="/about" component={About} />
+              <Route path="/chat" component={LiveChat} />
+              <Route path="/arena" component={Arena} />
+              <Route path="/battle" component={Battle} />
+              <Route path="/menu" component={Menu} />
+              <Route path="/mode" component={Mode} />
+              <Route path="/pokedex" component={Pokedex} />
+              <Route path="/rules" component={Rules} />
+              <Route path="/selection" component={Selection} />
+            </Switch>
+          </BrowserRouter>
+        ) : (
+          <div style={{ padding: "10px" }}>App Will Start Shortly ...</div>
+        )}
+      </div>
     );
   }
 }
 
 export default connect(mapStateToProps)(Router);
-
-
-
