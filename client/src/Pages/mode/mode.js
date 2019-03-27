3b13d27def06745a4284cf7e3a06db8aa166863c
Nicholas Mahase <nicholasmahase@gmail.com>
2019-03-27 00:52:25 -0400

    redirects

--- client/src/Pages/mode/mode.js
+++ client/src/Pages/mode/mode.js
@@ -9,7 +9,7 @@ export default class Mode extends Component {
        <h1>Choose Your Game Mode !</h1>
        </div>
        <div>
-       <a button type="button" id="singleBtn" className="btn btn-primary btn-lg btn-block" href="/selection">Single Player</a>
+       <a button type="button" id="singleBtn" className="btn btn-primary btn-lg btn-block" href="/battle">Single Player</a>
        <a button type="button" id="multiBtn" className="btn btn-primary btn-lg btn-block" href="/multi">MultiPlayer</a>
        </div>
        </div>
